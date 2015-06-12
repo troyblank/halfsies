78709ed7b0d17972e8047c539c8c0a966f89522f
troyblank <troy@troyblank.com>
2015-06-12 10:43:05 -0500

    added morgan + compression

--- config/express.js
+++ config/express.js
@@ -1,7 +1,16 @@
+var compress = require('compression');
 var express = require('express');
+var morgan = require('morgan');
 
 module.exports = function() {
     var app = express();
+
+    if(process.env.NODE_ENV === 'production'){
+        app.use(compress());
+    } else {
+        app.use(morgan('dev'));
+    }
+
     require('../app/routes/index.routes.js')(app);
     return app;
 };
