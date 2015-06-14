10ab679fd57ecd21585026725b50a232e085e58e
troyblank <troy@troyblank.com>
2015-06-14 11:48:07 -0500

    added mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('./config/express');
+var express = require('./config/express'),
+    mongoose = require('./config/mongoose');
 
 var app = express();
+var db = mongoose();
 var server = app.listen(8000);
+
 module.exports = app;
 
 console.log('Server running at http://localhost:8000/');
