024c944e19465c41eb4cf526bb7fa27cdfde25fd
troyblank <troy@troyblank.com>
2015-07-19 11:02:03 -0500

    conformed spec naming conventions

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,9 +2,9 @@ process.env.NODE_ENV = 'production';
 
 require('./app/self.spec');
 require('./app/server.spec');
-require('./app/controllers/index.controller.spec');
-require('./app/controllers/log.controller.spec');
-require('./app/controllers/user.controller.spec');
+require('./app/controllers/index.spec');
+require('./app/controllers/log.spec');
+require('./app/controllers/user.spec');
 require('./app/models/balance.spec');
 require('./app/models/log.spec');
 require('./app/models/user.spec');
