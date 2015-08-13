99e59e3659bf2c78a0c65978376c204eb9e486cd
troyblank <troy@troyblank.com>
2015-08-13 17:19:30 -0500

    updated signin styles

--- grunt/notify.js
+++ grunt/notify.js
@@ -4,5 +4,11 @@ module.exports = {
             title: 'Uglify',
             message: 'Uglify has compiled!'
         }
+    },
+    sass: {
+        options: {
+            title: 'Sass',
+            message: 'Sass has compiled!'
+        }
     }
 };
--- grunt/watch.js
+++ grunt/watch.js
@@ -3,6 +3,10 @@ module.exports = {
         files: ['app/assets/js/**/*.js'],
         tasks: ['uglify', 'notify:uglify']
     },
+    sass: {
+        files: ['app/assets/sass/**/*.scss'],
+        tasks: ['sass', 'notify:sass']
+    },
     ngtemplates : {
         files: ['app/views/partials/**/*.html'],
         tasks: ['ngtemplates']
