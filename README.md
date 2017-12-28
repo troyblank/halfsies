4a17d7f991b826581c387905e97480eff672df25
Troy Blank <troy.blank@topbox.io>
2017-12-27 19:26:48 -0600

    fixed crusty tests

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Halfies is an MEAN stack web app that keeps track of money owed between two mode
 * Node >= 0.12.6
 * MongoDB >= 3.04
 * Redis >= 3.0.3
-* Grunt >= 0.4.5
 
 ## Setup
 First thing you want to do is install all node packages run:
--- app/assets/js/views.js
+++ app/assets/js/views.js
@@ -2,135 +2,73 @@ angular.module('halfsies').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('app/views/partials/logCreate.html',
-    "<section class=\"page-wrap\">\r" +
-    "\n" +
-    "    <h1>Create a new Halfsie</h1>\r" +
-    "\n" +
-    "    <form data-ng-submit=\"create()\" novalidate>\r" +
-    "\n" +
-    "        <div>\r" +
-    "\n" +
-    "            <label for=\"amount\">Amount</label>\r" +
-    "\n" +
-    "            <div>\r" +
-    "\n" +
-    "                <input type=\"number\" data-ng-model=\"amount\" id=\"amount\" required />\r" +
-    "\n" +
-    "            </div>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "        <div>\r" +
-    "\n" +
-    "            <label for=\"description\">Description</label>\r" +
-    "\n" +
-    "            <div>\r" +
-    "\n" +
-    "                <textarea data-ng-model=\"description\" id=\"description\" cols=\"30\" rows=\"10\" required ></textarea>\r" +
-    "\n" +
-    "            </div>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "        <div class=\"alert alert__error icon-alert-error\" data-ng-show=\"error\">\r" +
-    "\n" +
-    "            <strong data-ng-bind=\"error\"></strong>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "        <div>\r" +
-    "\n" +
-    "            <input type=\"submit\" value=\"Submit\" data-pending-value=\"\" />\r" +
-    "\n" +
-    "            <a href=\"/#!/\" class=\"btn btn--alt\">Cancel</a>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "    </form>\r" +
-    "\n" +
+    "<section class=\"page-wrap\">\n" +
+    "    <h1>Create a new Halfsie</h1>\n" +
+    "    <form data-ng-submit=\"create()\" novalidate>\n" +
+    "        <div>\n" +
+    "            <label for=\"amount\">Amount</label>\n" +
+    "            <div>\n" +
+    "                <input type=\"number\" data-ng-model=\"amount\" id=\"amount\" required />\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "        <div>\n" +
+    "            <label for=\"description\">Description</label>\n" +
+    "            <div>\n" +
+    "                <textarea data-ng-model=\"description\" id=\"description\" cols=\"30\" rows=\"10\" required ></textarea>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "        <div class=\"alert alert__error icon-alert-error\" data-ng-show=\"error\">\n" +
+    "            <strong data-ng-bind=\"error\"></strong>\n" +
+    "        </div>\n" +
+    "        <div>\n" +
+    "            <input type=\"submit\" value=\"Submit\" data-pending-value=\"\" />\n" +
+    "            <a href=\"/#!/\" class=\"btn btn--alt\">Cancel</a>\n" +
+    "        </div>\n" +
+    "    </form>\n" +
     "</section>"
   );
 
 
   $templateCache.put('app/views/partials/overview.html',
-    "<section class=\"balance\" data-ng-controller=\"balance\" data-ng-show=\"balance\">\r" +
+    "<section class=\"balance\" data-ng-controller=\"balance\" data-ng-show=\"balance\">\n" +
+    "    <div class=\"page-wrap\">\n" +
+    "        <div class=\"balance__circle\" data-ng-class=\"{active: !balance.owedPrimary}\">\n" +
+    "            <div class=\"balance__circle-content\">\n" +
+    "                <div class=\"icon icon-balance-skull\"></div>\n" +
+    "                <div class=\"dollar-icon\">$</div>\n" +
+    "                <div class=\"number\">{{ balance.owedPrimary }}</div>\n" +
+    "                <div class=\"user\">{{ balance.userPrimary }}</div>\n" +
+    "            </div>\n" +
+    "            <div class=\"balance__circle-highlight\"><div></div></div>\n" +
+    "        </div>\n" +
+    "        <div class=\"balance__vs\">\n" +
+    "            <h1>Vs</h1>\n" +
+    "        </div>\n" +
+    "        <div class=\"balance__circle\" data-ng-class=\"{active: !balance.owedSecondary}\">\n" +
+    "            <div class=\"balance__circle-content\">\n" +
+    "                <div class=\"icon icon-balance-sheep\"></div>\n" +
+    "                <div class=\"dollar-icon\">$</div>\n" +
+    "                <div class=\"number\">{{ balance.owedSecondary }}</div>\n" +
+    "                <div class=\"user\">{{ balance.userSecondary }}</div>\n" +
+    "            </div>\n" +
+    "            <div class=\"balance__circle-highlight\"><div></div></div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</section>\n" +
+    "<div class=\"page-wrap\">\n" +
+    "    <a href=\"/#!/log/create\" class=\"btn\">Create a halfsie</a>\n" +
     "\n" +
-    "    <div class=\"page-wrap\">\r" +
-    "\n" +
-    "        <div class=\"balance__circle\" data-ng-class=\"{active: !balance.owedPrimary}\">\r" +
-    "\n" +
-    "            <div class=\"balance__circle-content\">\r" +
-    "\n" +
-    "                <div class=\"icon icon-balance-skull\"></div>\r" +
-    "\n" +
-    "                <div class=\"dollar-icon\">$</div>\r" +
-    "\n" +
-    "                <div class=\"number\">{{ balance.owedPrimary }}</div>\r" +
-    "\n" +
-    "                <div class=\"user\">{{ balance.userPrimary }}</div>\r" +
-    "\n" +
-    "            </div>\r" +
-    "\n" +
-    "            <div class=\"balance__circle-highlight\"><div></div></div>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "        <div class=\"balance__vs\">\r" +
-    "\n" +
-    "            <h1>Vs</h1>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "        <div class=\"balance__circle\" data-ng-class=\"{active: !balance.owedSecondary}\">\r" +
-    "\n" +
-    "            <div class=\"balance__circle-content\">\r" +
-    "\n" +
-    "                <div class=\"icon icon-balance-sheep\"></div>\r" +
-    "\n" +
-    "                <div class=\"dollar-icon\">$</div>\r" +
-    "\n" +
-    "                <div class=\"number\">{{ balance.owedSecondary }}</div>\r" +
-    "\n" +
-    "                <div class=\"user\">{{ balance.userSecondary }}</div>\r" +
-    "\n" +
-    "            </div>\r" +
-    "\n" +
-    "            <div class=\"balance__circle-highlight\"><div></div></div>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "    </div>\r" +
-    "\n" +
-    "</section>\r" +
-    "\n" +
-    "<div class=\"page-wrap\">\r" +
-    "\n" +
-    "    <a href=\"/#!/log/create\" class=\"btn\">Create a halfsie</a>\r" +
-    "\n" +
-    "\r" +
-    "\n" +
-    "    <ul data-ng-controller=\"logList\" class=\"log-list\" data-ng-show=\"logs\">\r" +
-    "\n" +
-    "        <li data-ng-repeat=\"log in logs\" data-ng-class=\"{negative: log.isNegative}\">\r" +
-    "\n" +
-    "            <span>{{ log.amount }}</span>\r" +
-    "\n" +
-    "            <span>{{ log.description }}</span>\r" +
-    "\n" +
-    "            <span>{{ log.user }}</span>\r" +
-    "\n" +
-    "        </li>\r" +
-    "\n" +
-    "        <span ng-show=\"(logs).length == 0\">No purchase log yet.</span>\r" +
-    "\n" +
-    "        <div data-ng-show=\"error\">\r" +
-    "\n" +
-    "            <strong data-ng-bind=\"error\"></strong>\r" +
-    "\n" +
-    "        </div>\r" +
-    "\n" +
-    "    </ul>\r" +
-    "\n" +
+    "    <ul data-ng-controller=\"logList\" class=\"log-list\" data-ng-show=\"logs\">\n" +
+    "        <li data-ng-repeat=\"log in logs\" data-ng-class=\"{negative: log.isNegative}\">\n" +
+    "            <span>{{ log.amount }}</span>\n" +
+    "            <span>{{ log.description }}</span>\n" +
+    "            <span>{{ log.user }}</span>\n" +
+    "        </li>\n" +
+    "        <span ng-show=\"(logs).length == 0\">No purchase log yet.</span>\n" +
+    "        <div data-ng-show=\"error\">\n" +
+    "            <strong data-ng-bind=\"error\"></strong>\n" +
+    "        </div>\n" +
+    "    </ul>\n" +
     "</div>"
   );
 
