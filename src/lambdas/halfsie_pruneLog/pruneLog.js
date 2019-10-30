3e5784f56f3a8a69c3c1e07e9684efb4413e47e0
Troy Blank <troy@troyblank.com>
2019-10-30 16:00:16 -0500

    added log prune lambda

---
+++ src/lambdas/halfsie_pruneLog/pruneLog.js
@@ -0,0 +1,19 @@
+const deleteLog = require('./deleteLog');
+
+function pruneLog(log, limit) {
+    return new Promise((resolve) => {
+        const toDelete = log.slice(limit, log.index);
+        const deletePromises = [];
+
+        toDelete.forEach((l) => {
+            const { id, date } = l;
+            deletePromises.push(deleteLog(id, date));
+        });
+
+        Promise.all(deletePromises).then(() => {
+            resolve();
+        });
+    });
+}
+
+module.exports = pruneLog;
